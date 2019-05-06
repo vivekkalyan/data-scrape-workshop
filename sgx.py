f306c077aa2cf30ab141a70d979689b70113a73b
Vivek Kalyan <hello@vivekkalyan.com>
2019-05-06 17:25:27 +0800

    Add example to get table data from sgx

---
+++ sgx.py
@@ -0,0 +1,46 @@
+import time
+import requests
+from bs4 import BeautifulSoup
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+## Install geckodriver
+#https://stackoverflow.com/questions/40867959/installing-geckodriver-only-using-terminal
+# wget https://github.com/mozilla/geckodriver/releases/download/v0.24.0/geckodriver-v0.24.0-linux64.tar.gz
+# tar -xvzf geckodriver-v0.11.1-linux64.tar.gz
+# rm geckodriver-v0.11.1-linux64.tar.gz
+# chmod +x geckodriver
+# cp geckodriver /usr/local/bin/
+
+
+# https://stackoverflow.com/questions/51046454/how-can-we-use-selenium-webdriver-in-colab-research-google-com
+firefox_options = webdriver.FirefoxOptions()
+firefox_options.add_argument('--headless')
+firefox_options.add_argument('--no-sandbox')
+firefox_options.add_argument('--disable-dev-shm-usage')
+driver = webdriver.Firefox(firefox_options=firefox_options)
+
+url = "https://www2.sgx.com/securities/company-announcements"
+driver.get(url)
+driver.implicitly_wait(5)
+driver.find_element_by_tag_name("table")
+res = driver.page_source
+driver.close()
+
+# res = requests.get(url)
+# soup = BeautifulSoup(res.content)
+
+soup = BeautifulSoup(res)
+all_rows = soup.find_all("tr")
+print(len(all_rows))
+# remove header row
+all_data = []
+for r in all_rows:
+    cols = r.find_all("td")
+    if len(cols) > 0:
+        row_data = []
+        for c in cols:
+            text = c.get_text()
+            row_data.append(text)
+        all_data.append(row_data)
