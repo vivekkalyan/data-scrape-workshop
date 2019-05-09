5edb1825029b79d02a80c76773e7ef757513adca
Vivek Kalyan <hello@vivekkalyan.com>
2019-05-09 22:12:22 +0800

    Remove unused import

--- sgx.py
+++ sgx.py
@@ -3,7 +3,6 @@ import requests
 from bs4 import BeautifulSoup
 
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 
 ## Install geckodriver
 #https://stackoverflow.com/questions/40867959/installing-geckodriver-only-using-terminal
